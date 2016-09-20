f43f2fbcd88393ab75c3b4f697afa3dae642eb00
Rachel Etnire <rachel.etnire@gmail.com>
2016-09-19 21:18:42 -0700

    add broken pop up to force fields to be filled

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,19 @@ $(function() {
 
     if (age > 14 && movie === 'casablanca' && gender === 'female') {
       $("#steve").show();
+
+    } else {
+        alert('Please enter your age before your true love will display.');
     }
+
+    if (age < 14) {
+      $("#taylor").show();
+    }
+
+    if (movie === 'starwars' || movie === 'schindler') {
+      $("#kanye").show();
+    }
+
 
   event.preventDefault();
   });
